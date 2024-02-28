d7388a503cdaffb3b737264c55457c9369278efb
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-28 03:45:41 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fetch = require('node-fetch');
-
 const app = express();
 const PORT = 3000;
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.get('/getRandomNeko', async (req, res) => {
     try {
         const apiResponse = await fetch('https://api.nekosapi.com/v3/images/random');
@@ -27,8 +29,6 @@ app.get('/getCharacter/:character_id', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-// Endpoint to get characters for a specific image
 app.get('/getCharactersForImage/:image_id', async (req, res) => {
     const imageId = req.params.image_id;
     try {
@@ -41,8 +41,6 @@ app.get('/getCharactersForImage/:image_id', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
-// Endpoint to get a list of all characters
 app.get('/getAllCharacters', async (req, res) => {
     try {
         const apiResponse = await fetch('https://api.nekosapi.com/v3/characters');
@@ -54,7 +52,6 @@ app.get('/getAllCharacters', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
 app.get('/getImagesForArtist/:artist_id', async (req, res) => {
     const artistId = req.params.artist_id;
     try {
@@ -67,7 +64,40 @@ app.get('/getImagesForArtist/:artist_id', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
+app.get('/ai/herc', async (req, res) => {
+    const prompt = req.query.prompt;
+  
+    // Check if prompt is provided
+    if (!prompt) {
+      return res.status(400).json({ error: 'Prompt is required' });
+    }
+  
+    try {
+      const response = await axios.get(`https://hercai.onrender.com/v3/hercai?question=${encodeURIComponent(prompt)}`);
+      res.json({ result: response.data });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  // Endpoint for text to image conversion using GET request
+  app.get('/ai/hercimg', async (req, res) => {
+    const prompt = req.query.prompt;
+  
+    // Check if text is provided
+    if (!prompt) {
+      return res.status(400).json({ error: 'prompt is required' });
+    }
+  
+    try {
+      const response = await axios.get(`https://hercai.onrender.com/v3/text2image?prompt=${encodeURIComponent(prompt)}`);
+      res.json({ result: response.data });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
